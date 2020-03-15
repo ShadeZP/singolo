6b0d14a198f06414f464a94c897ab3aadf681dc8
Evgeniy Vasilko <shadezp@gmail.com>
2020-03-15 15:43:24 +0200

    feat: add Get a quote popup

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const MENU = document.querySelector ('.navigation');
+const FORM = document.querySelector('.form');
+const CLOSE_BUTTON = document.querySelector('#btn-close');
+
+//HEADER
 
 MENU.addEventListener ('click', (event) => {
-    MENU.querySelectorAll('li').forEach(el => {
-        el.classList.remove ('navigation__text_active')
-    });
     const elem = event.target.parentNode;
-    elem.classList.add('navigation__text_active')
+    if (elem.className === 'navigation__text'){
+        MENU.querySelectorAll('li').forEach(el => {
+            el.classList.remove ('navigation__text_active')
+        });
+        console.log (event.target);
+        elem.classList.add('navigation__text_active')
+    }
+});
+
+// Get a quote
+
+FORM.addEventListener('submit',  event => {
+    const subject = FORM.querySelector('#subject-text');
+    const message = FORM.querySelector('.form__input_textarea');
+    event.preventDefault();
+    if (FORM.checkValidity()) {
+        document.querySelector('#popup-subject').innerText = (subject.value) ? `Тема: ${subject.value}` : `Без темы`;
+        document.querySelector('#popup-message').innerText = (message.value) ? `Описание: ${message.value}` : 'Без описания';
+        document.querySelector('.popup').classList.remove('popup_hidden');
+    };
+    FORM.reset();
+});
+
+CLOSE_BUTTON.addEventListener ('click',  (event) => {
+    if (event.target.id === 'btn-close'){
+        document.querySelector('#popup_msg').classList.add('popup_hidden');}
 });
